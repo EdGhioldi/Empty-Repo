dfcb4ed5333163a0b6ab0d9b5a810c3febecb6f0
Daniel Herrera RC <ramones_Rc@hotmail.com>
2023-07-21 20:37:23 -0300

    Finalizacion ManejoJDBC y Pool de Conexiones

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -1,14 +1,28 @@
 package datos;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
+import javax.sql.DataSource;
 
 public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimeZone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "admin";
 
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        //Definimos el tamaño inicial del Pool de Conexiones
+        ds.setInitialSize(5);
+        return ds;
+    }
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        return getDataSource().getConnection();
     }
     public static void close(ResultSet rs) throws SQLException {
         rs.close();
