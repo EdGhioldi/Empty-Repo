65bfb0dec44d2ac8b2ed4c108469ccf1f44a7ebf
EduardoGhioldi <ghioldigahonaeduardo@gmail.com>
2024-04-17 15:14:29 -0300

    application properties complete, The database connection was successfully established.

---
+++ src/main/java/com/dudicorp/demo/TcaApplication.java
@@ -0,0 +1,13 @@
+package com.dudicorp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dudicorp/demo/TcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudicorp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
