393b41ca16c177d2ffce1609ed7993b68a7f04e6
EduardoGhioldi <ghioldigahonaeduardo@gmail.com>
2024-04-17 15:55:38 -0300

    Se agregan las clases completas

--- src/main/java/com/dudicorp/demo/model/Curso.java
+++ src/main/java/com/dudicorp/demo/model/Curso.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,5 +20,15 @@ public class Curso {
     private String nombre;
     private String modalidad;
     private Date fecha_finalizacion;
+    private List<Tema> temaDelCurso;
 
+    public Curso(){};
+
+    public Curso(Long id_curso, String nombre, String modalidad, Date fecha_finalizacion, List<Tema> temaDelCurso) {
+        this.id_curso = id_curso;
+        this.nombre = nombre;
+        this.modalidad = modalidad;
+        this.fecha_finalizacion = fecha_finalizacion;
+        this.temaDelCurso = temaDelCurso;
+    }
 }
